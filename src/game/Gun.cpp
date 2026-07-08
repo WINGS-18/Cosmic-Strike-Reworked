bbe4f22b3576625a4103d6dba9c721084f293a7a
Wings <kelaskarmanoj18@gmail.com>
2026-07-08 23:19:50 +0530

    This stores the bullets into containers and moving them between each other to eliminate heap allocation

---
+++ src/game/Gun.cpp
@@ -0,0 +1,52 @@
+#include "game/Bullet.h"
+#include "game/Gun.h"
+
+
+namespace cs::Arsernal {
+
+    void Gun::bulletLoading(std::vector <Bullet>& bul) {
+        m_inactive = std::move(bul);
+    }
+
+    Bullet Gun::inactive_pop(const Eng::Vector2C& host) {
+        if(!m_inactive.empty()) {
+            Bullet b = m_inactive.back();
+            m_inactive.pop_back();
+            b.coordinateModifier(host);
+            return b;
+        }
+        return {0};
+    }
+
+    Bullet Gun::active_pop() {
+        if(!m_active.empty()) {
+            Bullet b = m_active.front();
+            m_active.pop_front();
+            return b;
+        }
+        return {0};
+    }
+
+    void Gun::inactive_push() {
+        Bullet b = active_pop();
+        if(b.m_val != 0) {
+            m_inactive.push_back(b);
+        }
+    }
+
+    void Gun::active_push(const Eng::Vector2C& host) {
+        Bullet b = inactive_pop(host);
+        if(b.m_val != 0) {
+            m_active.push_back(b);
+        }
+    }
+
+    void Gun::bulletAdder(const Eng::Vector2C& host, char key) {
+        if(key == ' ' && !m_inactive.empty()) {
+            active_push(host);
+        }else {
+            inactive_push();
+            active_push(host);
+        }
+    }
+}
