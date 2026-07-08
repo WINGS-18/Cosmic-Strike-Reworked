f3f13a436971048ddfd5c32633da17da5391ed7b
Wings <kelaskarmanoj18@gmail.com>
2026-07-08 23:18:54 +0530

    Added death condtion that checks the hp and sets isAlive flag

--- src/game/Character.cpp
+++ src/game/Character.cpp
@@ -2,8 +2,18 @@
 
 namespace cs {
 
+    //constructors
+
     Character::Character() {}
 
     Character::Character(std::vector<std::vector<int>> ed, bool isAl, Eng::Vector2C xny)
         : entityData(ed), isAlive(isAl), m_coord(xny) {}
+
+    const bool Character::getIsAlive() const {      //returns weather the character is alive or not
+        return isAlive;
+    }
+
+    const void Character::deathCondition() {
+        if(m_statsMan.m_current.m_hp <= 0)  isAlive = false;
+    }
 }
