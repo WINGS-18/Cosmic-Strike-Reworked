7dc750f26da07db7d59c0736e01a001b64586a77
Wings <kelaskarmanoj18@gmail.com>
2026-07-08 23:12:17 +0530

    Documented using comments about the motive of this header and added new functions, also making it's coordinates protected->public

--- include/game/Character.h
+++ include/game/Character.h
@@ -1,3 +1,15 @@
+/*
+@file Character.h
+
+* class Character:
+    - Every playble entities have some things in common like i'd,
+      alive or dead, stats(attack, defense etc.,), and x, y coordinates.
+    - Hence this class is a absract class not wrt c++ rules, but based on
+      it's behaviour.
+*/
+
+
+
 #pragma once
 
 #include "StatsPool.h"
@@ -9,14 +21,17 @@ namespace cs {
     protected:
         std::vector<std::vector<int>> entityData;     //holds the character layout info
         bool isAlive;
-        Eng::Vector2C m_coord;      //x and y coordinates
-        st::StatsPool m_base;         //stats manager
+        st::StatsPool m_statsMan;         //stats manager
     
     public:
+        Eng::Vector2C m_coord;      //x and y coordinates
 
+        //constructors
         Character();
-        
         Character(std::vector<std::vector<int>> ed, bool isAl, Eng::Vector2C xny);
 
+        const bool getIsAlive() const;      //returns weather character is alive or not
+
+        const void deathCondition();        //changes the m_isAlive flag
     };
 }
