8591dec7747ea500a7fbc3053a6afc5e8d014dc3
Wings <kelaskarmanoj18@gmail.com>
2026-07-08 23:08:26 +0530

    Added comments explaining the struct and also documented const keyword behaviour

--- include/engine/Vector2C.h
+++ include/engine/Vector2C.h
@@ -1,12 +1,41 @@
 /*
-A coustom structure instance capable of holding
-two coordinates namely x and y. 
-It also provides many overloaded operators that
-will help in easy comparions and also in making
-coordinate math easy
+* Eng is the common namespace
+  for the utilities even helpfull
+  to make other games.
+  */
+
+
+
+/*
+* @Vector2C.h.
+    - A struct instance capable of holding
+      two coordinates namely x and y. 
+    - It also provides many overloaded operators that
+      will help in easy comparions and also in arithematic
+      operations.
 */
 
+
+
 #pragma once
+
+
+/*
+$ 50 shades of const:
+
+* const datatype* var:
+    - The var pointer is strictly read only.
+    - Values or members cannot be atlered.
+
+* datatype* const var:
+    - The var pointer is read and write.
+    - The pointer var is locked and cannot be reassigned.
+
+* returntype function(datatype& para) const:
+    - This const at the end of the declaration gurrenttes the function will not change the members.
+    - Due to this complier can optimise by keeping the members in the registers.
+    - It reduces continous loading and unloading from RAM to registers.
+*/
 
 
 namespace Eng {
