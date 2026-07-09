b263a7753c85867c3e77fc672cdd3348dcd7636e
Wings <kelaskarmanoj18@gmail.com>
2026-07-09 22:39:17 +0530

    Enemy is implemented

---
+++ src/game/Enemy.cpp
@@ -0,0 +1,12 @@
+/*
+*@file Enemy.cpp.
+
+* This file is currently empty because
+  the base class Character and Enemy both
+  have same members and it even uses same
+  constructor. Not even new methods currently..
+*/
+
+
+
+#include "game/Enemy.h"
