50aa87d2bab1236a4fb98064c21d5225c1a39445
Wings <kelaskarmanoj18@gmail.com>
2026-07-11 22:17:55 +0530

    New utitlity namespace that gives access to ANSI codes and real time input from keyboard

---
+++ include/engine/Utilities.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace Utility {
+
+    void reset();
+    void green();
+    void red();
+
+    void clearScreen();
+    void hideCursor();
+    void showCursor();
+
+    char keyGiver();
+}
---
+++ src/engine/Utilities.cpp
@@ -0,0 +1,36 @@
+#include "engine/Utilities.h"
+#include <iostream>
+#include <conio.h>
+
+
+void Utility::reset() {
+    std::cout << "\033[0m";
+}
+
+void Utility::red() {
+    std::cout << "\033[31m";
+}
+
+void Utility::green() {
+    std::cout << "\033[92m";
+}
+
+void Utility::clearScreen() {
+    std::cout << "\033[2J\033[H";
+}
+
+void Utility::hideCursor() {
+    std::cout << "\033[?25l";
+}
+
+void Utility::showCursor() {
+    std::cout << "\033[?25h";
+}
+
+char Utility::keyGiver() {
+    if(_kbhit())
+        return _getch();
+
+    else
+        return '\0';
+}
