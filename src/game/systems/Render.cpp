cb6ff89865e2c49686047eed4c978e4edc1a02af
Wings <kelaskarmanoj18@gmail.com>
2026-07-31 21:00:35 +0530

    moved files to systems

---
+++ src/game/systems/Render.cpp
@@ -0,0 +1,61 @@
+#include "game/systems/Render.h"
+#include "engine/Utilities.h"
+#include "game/assets/GameData.h"
+#include "game/mechanics/Collision.h"
+#include <windows.h>
+#include <iostream>
+
+namespace cs {
+
+    Render::Render() {}     //constructor
+
+    Render& Render::getRenderer() {     //returns reference of it's own object
+        static Render r;
+        return r;
+    }
+
+    void Render::gameLoop(std::vector<Player>& ships, const std::vector<Part>& parts, std::vector<Enemy>& deadEnemies, std::vector<Enemy>& aliveEnemies) {
+        char key = '\0';
+        int count = 0;
+        col::Collision collisionCheck(GameData::makeEventPool());
+        Utility::hideCursor();
+        while(true) {
+            ships[0].gunFire();
+            frameReset();
+            if(count == 10) {
+                if(!deadEnemies.empty())
+                    GameData::enemyBirth(deadEnemies, aliveEnemies);
+                GameData::enemyMove(aliveEnemies);
+                count = 0;
+            }
+            key = Utility::keyGiver();
+            if(key == 'e')  break;
+            ships[0].userMovement(key);
+            std::cout << ships[0].getIsAlive() << std::endl;
+            collisionCheck.collisionHandler(aliveEnemies, ships);
+            insertEntity(ships, parts);
+            insertEntity(ships[0].m_playerGun.m_active, parts);
+            insertEntity(aliveEnemies, parts);
+            drawFrame();
+            Sleep(80);
+            Utility::clearScreen();
+            count++;
+        }
+        Utility::showCursor();
+    }
+
+    void Render::drawFrame() {      //draws the frame/grid
+        for(const auto& row : frame) {
+            for(const auto& cell : row) {
+                std::cout << cell;
+            }
+            std::cout << "\n";
+        }
+    }
+
+    void Render::frameReset() {     //resets the frame with blank space chars
+        for(auto& row : frame) {
+            std::fill(row.begin(), row.end(), ' ');
+        }
+    }
+}
