fac86e172f35ef83bca780475c67c6587be88d10
Wings <kelaskarmanoj18@gmail.com>
2026-08-03 22:17:06 +0530

    The parameterised constructor now works perfectly

--- src/game/assets/StatsPool.cpp
+++ src/game/assets/StatsPool.cpp
@@ -2,9 +2,10 @@
 
 namespace cs::st {
 
-    StatsPool::StatsPool(const std::vector<std::vector<int>>& entityData, const std::vector<Part>& parts) {
-        m_base.totalStats(entityData, parts);
-        m_current.totalStats(entityData, parts);
-    }
+    StatsPool::StatsPool(const std::vector<std::vector<int>>& entityData, const std::vector<Part>& parts)
+        : m_base(entityData, parts)
+        {
+            m_current = m_base;
+        }
 
 }
