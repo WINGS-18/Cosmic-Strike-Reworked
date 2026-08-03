95de887167dc4fb91b66f9877e14c1cc75e2e3a6
Wings <kelaskarmanoj18@gmail.com>
2026-08-03 21:22:41 +0530

    Implemented a node class and it's functions

---
+++ src/gameui/Menu.cpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "include/gameui/Menu.h"
+#include <iostream>
+
+
+namespace menu {
+
+    Node::Node(std::string name)
+        : m_menuName(std::move(name)) {}
+
+    void Node::display() {
+        for(int i = 0; const auto& child : m_children) {
+            std::cout << static_cast<char>('a' + i) << ")" << child->m_menuName << std::endl;
+            i++;
+        }
+    }
+
+}
