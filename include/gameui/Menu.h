a6fd9467eee8875245e843dde3b8ef9bc8d53ea6
Wings <kelaskarmanoj18@gmail.com>
2026-08-06 22:39:51 +0530

    refactor(gameui): migrate Warehouse class to flat 1D parts array using move semantics

--- include/gameui/Menu.h
+++ include/gameui/Menu.h
@@ -3,7 +3,9 @@
 
 #include <vector>
 #include <string>
-
+#include <memory>
+#include "game/assets/GameData.h"
+#include "engine/Slot.h"
 
 namespace menu {
 
@@ -12,7 +14,7 @@ namespace menu {
     class Node {
     protected:
         std::string m_menuName;
-        std::vector<Node*> m_children;
+        std::vector<std::unique_ptr<Node>> m_children;
 
     public:
         Node() = default;       //default constructor
@@ -22,11 +24,21 @@ namespace menu {
 
         virtual void display();
 
-        std::vector<Node*>& getChildren();
+        std::vector<Node*> getChildren();
+
+        void addChild(std::unique_ptr<Node> child);
     };
 
-    void addNode(Node* parent, Node* child);
 
-    class Warehouse : public 
+    class Warehouse : public Node {
+    private:
+        std::vector<cs::Part> m_parts;
+        Eng::Slot m_placement;
+        std::vector<Eng::Slot> m_allParts;
+    public:
+        Warehouse(std::string name, std::vector<cs::Part> parts, Eng::Slot place, std::vector<Eng::Slot> allParts);
+
+        void display() override;
+    };
 
 }
