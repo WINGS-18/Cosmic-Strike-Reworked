f2dbae9ea8367e59459131f7ae3f55b1ff5e8b3c
Wings <kelaskarmanoj18@gmail.com>
2026-08-08 23:56:11 +0530

    Implemented arrow class. Arrow class provides an arrow pointing at left of every menu section which makes it interactive way for selecting them

---
+++ src/gameui/Arrow.cpp
@@ -0,0 +1,61 @@
+#include "gameui/Arrow.h"
+#include <iostream>
+
+namespace menu {
+
+    Arrow::Arrow(std::vector<char> navigArrow)
+        : m_navigArrow(std::move(navigArrow)) {}
+
+    const std::vector<char>& Arrow::getArrow() const {
+        return m_navigArrow;
+    }
+
+    char Arrow::printNavigArrow(int index) const {
+        return m_navigArrow[index];
+    }
+
+    void Arrow::setButtonPressed(char key) {
+        std::cout << key;
+        switch (key) {
+            case 13 :
+                m_pressed = Buttons::ENTER;
+                break;
+
+            case 'a' :
+                m_pressed = Buttons::PAGE_UP;
+                break;
+
+            case 'b' :
+                m_pressed = Buttons::PAGE_DOWN;
+                break;
+            
+            default:
+                break;
+        }
+    }
+
+    void Arrow::moveArrow(int start, int end) {
+        switch (m_pressed) {
+            case Buttons::ENTER :
+            m_arrIndex = 0;
+            break;
+            
+            case Buttons::PAGE_UP :
+            if(m_arrIndex != start) {
+                std::swap(m_navigArrow[m_arrIndex], m_navigArrow[--m_arrIndex]);
+            }
+            break;
+            
+            case Buttons::PAGE_DOWN :
+            if(m_arrIndex != end) {
+                std::swap(m_navigArrow[m_arrIndex], m_navigArrow[m_arrIndex + 1]);
+                std::cout << "move : " << m_navigArrow[m_arrIndex] << "\n";
+            }
+            break;
+
+        default:
+            break;
+        }
+    } 
+
+}
