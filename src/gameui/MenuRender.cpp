9f88edb10678c7ab035a327d9ce600569b30518a
Wings <kelaskarmanoj18@gmail.com>
2026-08-09 22:58:32 +0530

    moveArrow now returns an integer which is a m_arrIndex. * This is used to navigate into the specific child node

--- src/gameui/MenuRender.cpp
+++ src/gameui/MenuRender.cpp
@@ -5,12 +5,19 @@ namespace menu {
     MenuRender::MenuRender() {}
 
     void MenuRender::menuLoop(std::unique_ptr<Node> root, Arrow& arrow) {
+        m_navigStack.push(root.get());
         while(true) {
-            root->display(arrow);
+            if(!m_navigStack.empty()) {
+                auto& top = m_navigStack.top();
+                top->display(arrow);
+            }
             char control = Utility::pressKey();
             if(control == 'e')  break;
             arrow.setButtonPressed(control);
-            arrow.moveArrow(0, root->getChildren().size() - 1);
+            int frame = arrow.moveArrow(0, m_navigStack.top()->getChildren().size() - 1);
+            if(frame != -1) {
+                m_navigStack.push(root->getChildren()[frame]);
+            }
             Utility::clearScreen();
         }
     }
