f9f8b377166d13f5f805348af426fd11aa708e16
Wings <kelaskarmanoj18@gmail.com>
2026-08-19 19:03:08 +0530

    The constructor takes Arsenal::Gun parameter by value for using proper move semantics

--- include/game/entities/Player.h
+++ include/game/entities/Player.h
@@ -18,7 +18,7 @@ namespace cs {
         Arsenal::Gun m_playerGun;
         
         //constructor
-        Player(std::vector<std::vector<int>> ed, bool isAl, Eng::Vector2C xny, Arsenal::Gun& g, const std::vector<Part>& parts);
+        Player(std::vector<std::vector<int>> ed, bool isAl, Eng::Vector2C xny, Arsenal::Gun g, const std::vector<Part>& parts);
 
         void userMovement(char key);        //makes user move or interact based on the keyboard inputs
 
