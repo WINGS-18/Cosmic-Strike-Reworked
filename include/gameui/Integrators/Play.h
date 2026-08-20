38c75aacd0d1641fa6804e3d7bcde75349b01777
Wings <kelaskarmanoj18@gmail.com>
2026-08-20 22:07:04 +0530

    Play class builds bridge between gameplay and menu selection

---
+++ include/gameui/Integrators/Play.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "game/systems/Render.h"
+
+namespace Integrator {
+
+    class Play {
+    public:
+        Play() = default;
+
+        cs::Player getPlayer(std::vector<std::vector<int>> entity, bool changed, const std::vector<cs::Part>& parts);
+        void makeEnemies(std::vector<cs::Enemy>& deadEnemies);
+        void startGame(std::vector<std::vector<int>>& entity, bool changed) {}
+    };
+
+}
