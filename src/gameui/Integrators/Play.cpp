572d6d11b5ba48fa79ab1d0da3773e1c8b0efc48
Wings <kelaskarmanoj18@gmail.com>
2026-08-23 22:56:14 +0530

    Added new functions

--- src/gameui/Integrators/Play.cpp
+++ src/gameui/Integrators/Play.cpp
@@ -11,5 +11,20 @@ namespace Integrator {
         }else {
             return cs::Player(std::move(cs::GameData::getUserLayout()), true, {0, 24}, std::move(g), parts);
         }
-    } 
+    }
+
+    void Play::makeEnemies(std::vector<cs::Enemy>& deadEnemies, const std::vector<cs::Part>& parts) {
+        cs::GameData::enemyMaker(deadEnemies, cs::GameData::getEnemyLayout(), cs::GameData::xCoordinateMaker(10), parts, 0);
+    }
+
+    void Play::startGame(std::vector<std::vector<int>>& entity, bool changed) {
+        auto parts = cs::GameData::getParts();
+        auto player = getPlayer(entity, changed, parts);
+        std::vector<cs::Player> ships = {player};
+        std::vector<cs::Enemy> aliveEnemies;
+        std::vector<cs::Enemy> deadEnemies;
+        makeEnemies(deadEnemies, parts);
+        cs::Render::getRenderer().gameLoop(ships, parts, deadEnemies, aliveEnemies);
+    }
+
 }
