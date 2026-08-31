f1cad00db1b917f3fbdabb627753f7e7cb4ff792
Wings <kelaskarmanoj18@gmail.com>
2026-08-31 20:20:46 +0530

    Added something which i don't remember

--- include/gameui/Integrators/Play.h
+++ include/gameui/Integrators/Play.h
@@ -9,8 +9,8 @@ namespace Integrator {
         Play() = default;
 
         cs::Player getPlayer(std::vector<std::vector<int>> entity, bool changed, const std::vector<cs::Part>& parts);
-        void makeEnemies(std::vector<cs::Enemy>& deadEnemies);
-        void startGame(std::vector<std::vector<int>>& entity, bool changed) {}
+        void makeEnemies(std::vector<cs::Enemy>& deadEnemies, const std::vector<cs::Part>& parts);
+        void startGame(std::vector<std::vector<int>>& entity, bool changed);
     };
 
 }
