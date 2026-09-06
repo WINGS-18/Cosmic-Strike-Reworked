6958723e3d7e05bb5ba3d53c71f6f50974475034
Wings <kelaskarmanoj18@gmail.com>
2026-09-06 21:28:18 +0530

    *Implemented reload function that triggers reload on a gun after m_currAmmo becomes 0. * Implemented isReloading() function that returns a boolean by checking State a of the magazine.

--- src/game/arsenal/Magazine.cpp
+++ src/game/arsenal/Magazine.cpp
@@ -4,8 +4,9 @@ namespace cs {
 
     namespace Arsenal {
 
-        Magazine::Magazine(int maxAmmo)
-            : m_currAmmo(maxAmmo), m_maxAmmo(maxAmmo) {}
+        Magazine::Magazine(int maxAmmo, int reloadTime)
+            : m_currAmmo(maxAmmo), m_maxAmmo(maxAmmo),
+              m_reloadTimeInFrames(reloadTime), m_currReloadTimeInFrames(0) {}
 
         void Magazine::setState() {
             if(m_currAmmo > 0) {
@@ -17,6 +18,25 @@ namespace cs {
 
         void Magazine::decrementMagazineAmmo() noexcept{
             if(m_currState == State::ready)  m_currAmmo--;
+        }
+
+        void Magazine::reload() noexcept {
+            if(m_currState == Magazine::State::reloading) {
+                m_currReloadTimeInFrames++;
+            }
+
+            if(m_currReloadTimeInFrames == m_reloadTimeInFrames) {
+                m_currReloadTimeInFrames = 0;
+                m_currAmmo = m_maxAmmo;
+            }
+        }
+
+        bool Magazine::isReloading() const noexcept {
+            if(m_currState == State::reloading) {
+                return true;
+            }
+
+            return false;
         }
 
     }
